2a80d76b3c24ec1dc5c72fe9ee7ba2d3942525c0
Balfre <35463055+Balfre@users.noreply.github.com>
2018-01-22 11:37:27 -0600

    Add files via upload

---
+++ grafica.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+import matplotlib.pyplot as plt
+plt.plot([0,0,1,2,1,2,2,2,3,4,4,3,4,5,5,5,6,6,6,7,8])
+plt.title('Balfre')
+plt.xlabel('Numero de años')
+plt.ylabel('Numero de mascotas y amigos [UA]')
+plt.savefig('Grafica con titulos.png')
+plt.show('Grafica con titulos.png')
+
